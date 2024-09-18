52a69ca40f37435df9ec4c6b1bd8a599daedfaa8
Muhammad Asif <70218962+AloneAsif@users.noreply.github.com>
2024-09-18 14:37:34 +0500

    Add files via upload

--- milestone_3/main.js
+++ milestone_3/main.js
@@ -1,16 +1,23 @@
 var get_form = document.getElementById('resume_form');
 get_form.addEventListener('submit', function (event) {
+    var _a;
     event.preventDefault();
     // Define Data
+    var profile_picture = document.getElementById("profile_picture");
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
     var education = document.getElementById('education').value;
     var experience = document.getElementById('experience').value;
     var Skills = document.getElementById('Skills').value;
-    var btn = document.getElementById('button');
+    var profile_picture_file = (_a = profile_picture.files) === null || _a === void 0 ? void 0 : _a[0];
+    var profile_picture_url = profile_picture_file
+        ? URL.createObjectURL(profile_picture_file)
+        : "";
     var resumeDisplay = document.getElementById('resume-display');
-    resumeDisplay.innerHTML = "\n        <h2>".concat(name, "</h2>\n        <p><strong>Email:</strong> ").concat(email, "</p>\n        <p><strong>Phone:</strong> ").concat(phone, "</p>\n        <p><strong>education:</strong> ").concat(education, "</p>\n        <p><strong>experience:</strong> ").concat(experience, "</p>\n        <p><strong>Skills:</strong> ").concat(Skills, "</p>\n        \n    ");
+    resumeDisplay.innerHTML = "\n    ".concat(profile_picture_url
+        ? "<img src=\"".concat(profile_picture_url, "\" alt=\"profile_picture\" class=\"profilePicture\">")
+        : "", "\n        <h2>").concat(name, "</h2>\n        <p><strong>Email:</strong> ").concat(email, "</p>\n        <p><strong>Phone:</strong> ").concat(phone, "</p>\n        <p><strong>education:</strong> ").concat(education, "</p>\n        <p><strong>experience:</strong> ").concat(experience, "</p>\n        <p><strong>Skills:</strong> ").concat(Skills, "</p>\n        \n    ");
     console.log({
         name: name,
         email: email,
--- milestone_3/main.ts
+++ milestone_3/main.ts
@@ -3,7 +3,9 @@ get_form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Define Data
-    
+    const profile_picture = document.getElementById(
+        "profile_picture"
+      ) as HTMLInputElement;
     const name = (document.getElementById('name') as HTMLInputElement).value;
     const email = (document.getElementById('email') as HTMLInputElement).value;
     const phone = (document.getElementById('phone') as HTMLInputElement).value;
@@ -12,8 +14,18 @@ get_form.addEventListener('submit', function(event) {
     const Skills = (document.getElementById('Skills') as HTMLInputElement).value;
 
 
+    const profile_picture_file = profile_picture.files?.[0];
+  const profile_picture_url = profile_picture_file
+    ? URL.createObjectURL(profile_picture_file)
+    : "";
+
     const resumeDisplay = document.getElementById('resume-display')!;
     resumeDisplay.innerHTML = `
+    ${
+          profile_picture_url
+            ? `<img src="${profile_picture_url}" alt="profile_picture" class="profilePicture">`
+            : ""
+        }
         <h2>${name}</h2>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Phone:</strong> ${phone}</p>
